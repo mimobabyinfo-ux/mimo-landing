915fb41f589d12342d9388aed89e85ea492d7d61
yahavadmon <yahav.admon@buildots.com>
2026-07-10 17:30:21 +0300

    hotfix: restore truncated main.tsx render + thank-you closing tags

--- src/main.tsx
+++ src/main.tsx
@@ -4,4 +4,11 @@ import App from './App.tsx'
 import './index.css'
 import { initWhatsAppClickTracking } from './lib/track'
 
-// Fire the Meta pixel custom event "whatsapp_click" for every w
+// Fire the Meta pixel custom event "whatsapp_click" for every wa.me link on the page.
+initWhatsAppClickTracking()
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
